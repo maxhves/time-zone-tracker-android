bd938482d088a2bd024efb4b804b6b840bfd5fec
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-03-23 11:26:03 +1300

    Added styling to saved timezone item components.

--- feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/component/SavedTimezoneItem.kt
+++ feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/component/SavedTimezoneItem.kt
@@ -1,34 +1,53 @@
 package uk.co.mhl.timezonetracker.feature.timezones.component
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import uk.co.mhl.timezonetracker.core.designsystem.theme.TimezoneTrackerTheme
 
 @Composable
 internal fun SavedTimezoneItem(
     modifier: Modifier = Modifier,
+    cityName: String,
+    offset: Int,
+    currentTime: Long,
 ) {
     Column(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable {  }
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        Text(text = "Toronto")
+        Text(
+            text = cityName,
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onBackground.copy(alpha = .75f)
+        )
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween,
         ) {
             Text(
-                text = "04:45 AM",
+                text = "$currentTime",
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.onBackground,
             )
             Text(
-                text = "-05:00 EST",
+                text = "$offset",
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onBackground.copy(alpha = .5f)
             )
         }
     }
@@ -38,6 +57,10 @@ internal fun SavedTimezoneItem(
 @Composable
 private fun SavedTimezoneItemPreview() {
     TimezoneTrackerTheme {
-        SavedTimezoneItem()
+        SavedTimezoneItem(
+            cityName = "Toronto",
+            offset = -5,
+            currentTime = 9883908928,
+        )
     }
 }

9ce632eb997a4d26fbd34c3ee3b3a790952db915
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-03-22 17:45:03 +1300

    Added new color palette.

--- feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/TimezonesScreen.kt
+++ feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/TimezonesScreen.kt
@@ -1,17 +1,20 @@
 package uk.co.mhl.timezonetracker.feature.timezones
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import uk.co.mhl.timezonetracker.core.designsystem.theme.Purple40
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TimezonesScreen(
     modifier: Modifier = Modifier,
 ) {
     Text(
-        modifier = modifier,
+        modifier = modifier.background(MaterialTheme.colorScheme.primary).padding(16.dp),
         text = "Hello from the Timezones screen",
-        color = Purple40,
+        color = MaterialTheme.colorScheme.onPrimary,
     )
 }

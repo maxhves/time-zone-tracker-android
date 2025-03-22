88c498bc5a208c080b689d3d4c5c8a4845f4c16e
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-03-22 18:34:05 +1300

    Setup timezones screen preview.

--- feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/TimezonesScreen.kt
+++ feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/TimezonesScreen.kt
@@ -1,10 +1,12 @@
 package uk.co.mhl.timezonetracker.feature.timezones
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun TimezonesScreen(
@@ -25,6 +27,17 @@ internal fun TimezonesScreen(
     Scaffold { innerPadding ->
         Text(
             modifier = Modifier.padding(innerPadding), text = "Hello"
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TimezonesScreenPreview() {
+    MaterialTheme {
+        TimezonesScreen(
+            savedTimezones = emptyList(),
+            modifier = Modifier,
         )
     }
 }

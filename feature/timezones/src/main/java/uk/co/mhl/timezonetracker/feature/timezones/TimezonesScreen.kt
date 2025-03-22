386a650c4264a5000b9a3871af194daa7ca48ccb
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-03-22 19:13:12 +1300

    Setup initial timezone tracker top app bar.

--- feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/TimezonesScreen.kt
+++ feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/TimezonesScreen.kt
@@ -1,15 +1,16 @@
 package uk.co.mhl.timezonetracker.feature.timezones
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import uk.co.mhl.timezonetracker.core.designsystem.component.TimezoneTrackerTopAppBar
 import uk.co.mhl.timezonetracker.core.designsystem.theme.TimezoneTrackerTheme
-import uk.co.mhl.timezonetracker.feature.timezones.components.NewTimezoneFloatingActionButton
+import uk.co.mhl.timezonetracker.feature.timezones.component.NewTimezoneFloatingActionButton
 
 @Composable
 fun TimezonesScreen(
@@ -23,6 +24,7 @@ fun TimezonesScreen(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun TimezonesScreen(
     savedTimezones: List<String>,
@@ -30,6 +32,11 @@ internal fun TimezonesScreen(
     modifier: Modifier = Modifier,
 ) {
     Scaffold(
+        topBar = {
+            TimezoneTrackerTopAppBar(
+                titleRes = R.string.timezones_screen_title,
+            )
+        },
         floatingActionButton = { NewTimezoneFloatingActionButton(onClick = onNewTimezoneClick) },
         floatingActionButtonPosition = FabPosition.Center,
     ) { innerPadding ->

9108d7779d8e5bd41050fed455ffcf6ba1b300b5
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-03-24 17:20:34 +1300

    Replaced CurrentTimeDataSource with TimeTickBroadcastMonitor.

---
+++ core/local/src/main/java/uk/co/mhl/timezonetracker/core/local/TimeTickBroadcastMonitor.kt
@@ -0,0 +1,59 @@
+package uk.co.mhl.timezonetracker.core.local
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.shareIn
+import java.time.Instant
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+internal class TimeTickBroadcastMonitor @Inject constructor(
+    @ApplicationContext private val context: Context,
+    private val appScope: CoroutineScope = CoroutineScope(Dispatchers.Default),
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+) : TimeTickMonitor {
+    override fun currentTime(): SharedFlow<Instant> = callbackFlow {
+        trySend(Instant.now())
+
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(cotext: Context, intent: Intent) {
+                if (intent.action != Intent.ACTION_TIME_TICK) return
+
+                val timeFromIntent = if (VERSION.SDK_INT < VERSION_CODES.R) {
+                    null
+                } else {
+                    intent.getStringExtra(Intent.EXTRA_TIME)?.let { time ->
+                        Instant.ofEpochMilli(time.toLong())
+                    }
+                }
+
+                trySend(timeFromIntent ?: Instant.now())
+            }
+        }
+        context.registerReceiver(receiver, IntentFilter(Intent.ACTION_TIME_TICK))
+
+        awaitClose {
+            context.unregisterReceiver(receiver)
+        }
+    }
+        .distinctUntilChanged()
+        .conflate()
+        .flowOn(ioDispatcher)
+        .shareIn(appScope, SharingStarted.WhileSubscribed(5_000, 1))
+}
--- core/local/src/main/java/uk/co/mhl/timezonetracker/core/local/TimeTickMonitor.kt
+++ core/local/src/main/java/uk/co/mhl/timezonetracker/core/local/TimeTickMonitor.kt
@@ -3,6 +3,6 @@ package uk.co.mhl.timezonetracker.core.local
 import kotlinx.coroutines.flow.Flow
 import java.time.Instant
 
-interface TimeDataSource {
-    fun getCurrentTime(): Flow<Instant>
+interface TimeTickMonitor {
+    fun currentTime(): Flow<Instant>
 }

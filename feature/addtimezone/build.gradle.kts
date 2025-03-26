cf53a089c5d75efe4d1e0be6541af846e0e0cac3
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-03-26 20:50:50 +1300

    Added feature/addtimezone module

---
+++ feature/addtimezone/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.timezonetracker.android.feature)
+    alias(libs.plugins.timezonetracker.android.library.compose)
+}
+
+android {
+    namespace = "uk.co.mhl.timezonetracker.feature.addtimezone"
+}
+
+dependencies {
+    implementation(project(":core:data"))
+}

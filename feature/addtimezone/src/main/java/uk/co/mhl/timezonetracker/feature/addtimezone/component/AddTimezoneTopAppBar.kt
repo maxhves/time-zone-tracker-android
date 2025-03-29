b3b32f0a71efcfe617e29cc6f3ed9a517ee5e88d
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-03-29 17:36:59 +1300

    Added initial AddTimezoneTopAppBar component.

---
+++ feature/addtimezone/src/main/java/uk/co/mhl/timezonetracker/feature/addtimezone/component/AddTimezoneTopAppBar.kt
@@ -0,0 +1,44 @@
+package uk.co.mhl.timezonetracker.feature.addtimezone.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import uk.co.mhl.timezonetracker.core.designsystem.theme.TimezoneTrackerTheme
+import uk.co.mhl.timezonetracker.feature.addtimezone.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun AddTimezoneTopAppBar(
+    onNavigateClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    TopAppBar(
+        title = { },
+        modifier = modifier,
+        navigationIcon = {
+            IconButton(onNavigateClick) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+                    contentDescription = stringResource(R.string.navigate_up)
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun AddTimezoneTopAppBarPreview() {
+    TimezoneTrackerTheme {
+        AddTimezoneTopAppBar(
+            onNavigateClick = { }
+        )
+    }
+}

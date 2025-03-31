3accb4a63761c94b120a14f59edddaf598ecb53c
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-03-31 15:46:07 +1300

    Grouped cities by first character, alphabetically.

--- feature/addtimezone/src/main/java/uk/co/mhl/timezonetracker/feature/addtimezone/AddTimezoneScreen.kt
+++ feature/addtimezone/src/main/java/uk/co/mhl/timezonetracker/feature/addtimezone/AddTimezoneScreen.kt
@@ -1,16 +1,22 @@
 package uk.co.mhl.timezonetracker.feature.addtimezone
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,34 +41,57 @@ internal fun AddTimezoneScreen(
     )
 }
 
+@OptIn(
+    ExperimentalMaterial3Api::class,
+    ExperimentalFoundationApi::class
+)
 @Composable
 internal fun AddTimezoneScreen(
-    cities: List<City>,
+    cities: Map<Char, List<City>>,
     onCitySelected: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+
     Scaffold(
-        modifier = modifier,
-        topBar = { AddTimezoneTopAppBar(onCitySelected) }
+        topBar = { AddTimezoneTopAppBar(onCitySelected, scrollBehavior = scrollBehavior) },
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
     ) { innerPadding ->
         LazyColumn(
-            modifier = modifier.padding(innerPadding),
+            modifier = Modifier.padding(innerPadding),
         ) {
-            items(
-                items = cities,
-                key = { city -> city.id }
-            ) { city ->
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable(onClick = onCitySelected)
-                        .padding(16.dp),
-                    text = "${city.name}, ${city.country}",
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                )
+            cities.forEach { (character, citiesForChar) ->
+                stickyHeader {
+                    Box(
+                        modifier = Modifier
+                            .background(MaterialTheme.colorScheme.background)
+                            .fillMaxWidth()
+                            .padding(16.dp, 8.dp),
+                    ) {
+                        Text(
+                            text = "$character",
+                            style = MaterialTheme.typography.labelMedium,
+                            color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.75f)
+                        )
+                    }
+                }
+
+                items(
+                    items = citiesForChar,
+                    key = { city -> city.id }
+                ) { city ->
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable(onClick = onCitySelected)
+                            .padding(16.dp),
+                        text = "${city.name}, ${city.country}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                }
             }
         }
     }
@@ -73,9 +102,10 @@ internal fun AddTimezoneScreen(
 private fun AddTimezoneScreenPreview() {
     TimezoneTrackerTheme {
         AddTimezoneScreen(
-            cities = listOf(
-                City(id = 1, name = "Toronto", country = "Canada", zoneId = "America/Toronto"),
-                City(id = 2, name = "Oslo", country = "Norway", zoneId = "Europe/Oslo"),
+            cities = mapOf(
+                'L' to listOf(City(id = 1, name = "London", country = "United Kingdom", zoneId = "Europe/London")),
+                'O' to listOf(City(id = 2, name = "Oslo", country = "Norway", zoneId = "Europe/Oslo")),
+                'T' to listOf(City(id = 3, name = "Toronto", country = "Canada", zoneId = "America/Toronto")),
             ),
             onCitySelected = { },
         )

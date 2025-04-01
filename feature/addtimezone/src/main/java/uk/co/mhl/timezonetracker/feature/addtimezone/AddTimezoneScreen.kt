6c1e29e6559a2a2d7399b09c8dee001b2b046c34
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-04-01 16:20:00 +1300

    Added night themes file with night theme declaration.

--- feature/addtimezone/src/main/java/uk/co/mhl/timezonetracker/feature/addtimezone/AddTimezoneScreen.kt
+++ feature/addtimezone/src/main/java/uk/co/mhl/timezonetracker/feature/addtimezone/AddTimezoneScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme

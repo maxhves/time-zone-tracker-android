1784fdb0c31dfbb786b3fa38e127f64672701c80
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-04-02 18:40:33 +1300

    Added DefaultCityRepository test class along with fake CityDao.

---
+++ core/data/src/test/java/uk/co/mhl/timezones/core/data/core/data/repository/DefaultCityRepositoryTest.kt
@@ -0,0 +1,47 @@
+package uk.co.mhl.timezones.core.data.core.data.repository
+
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import uk.co.mhl.timezones.core.data.core.data.testdoubles.TestCityDao
+import uk.co.mhl.timezonetracker.core.data.repository.DefaultCityRepository
+import uk.co.mhl.timezonetracker.core.database.dao.CityDao
+import kotlin.test.assertEquals
+
+class DefaultCityRepositoryTest {
+
+    private val testScope = TestScope(UnconfinedTestDispatcher())
+
+    private lateinit var subject: DefaultCityRepository
+
+    private lateinit var cityDao: CityDao
+
+    @Before
+    fun setup() {
+        cityDao = TestCityDao()
+
+        subject = DefaultCityRepository(
+            cityDataSource = cityDao,
+        )
+    }
+
+    @Test
+    fun defaultCityRepository_observe_ids_returns_correct_cities() = testScope.runTest {
+        val cities = subject.observeByIds(setOf(2)).first()
+
+        assertEquals(
+            listOf(2),
+            cities.map { city -> city.id }
+        )
+    }
+
+    @Test
+    fun defaultCityRepository_get_all_returns_all_cities() = testScope.runTest {
+        val cities = subject.getAll()
+
+        assertEquals(3, cities.count())
+    }
+}
---
+++ core/data/src/test/java/uk/co/mhl/timezones/core/data/core/data/testdoubles/TestCityDao.kt
@@ -0,0 +1,41 @@
+package uk.co.mhl.timezones.core.data.core.data.testdoubles
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import uk.co.mhl.timezonetracker.core.database.dao.CityDao
+import uk.co.mhl.timezonetracker.core.database.model.LocalCity
+
+class TestCityDao : CityDao {
+    private val citiesStateFlow = MutableStateFlow(
+        listOf(
+            LocalCity(
+                id = 1,
+                name = "London",
+                country = "England",
+                zoneId = "Europe/London",
+            ),
+            LocalCity(
+                id = 2,
+                name = "Toronto",
+                country = "Canada",
+                zoneId = "America/Toronto",
+            ),
+            LocalCity(
+                id = 3,
+                name = "Auckland",
+                country = "New Zealand",
+                zoneId = "Pacific/Auckland"
+            )
+        )
+    )
+
+    override fun observeByIds(ids: Set<Int>): Flow<List<LocalCity>> {
+        return citiesStateFlow.map { cities -> cities.filter { city -> city.id in ids } }
+    }
+
+    override suspend fun getAll(): List<LocalCity> {
+        return citiesStateFlow.first()
+    }
+}

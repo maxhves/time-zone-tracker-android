861adddf8087e719eac75160207db943905d032b
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-04-06 14:36:17 +1200

    Setup further necessary dependencies.

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.internal.extensions.stdlib.capitalized
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     alias(libs.plugins.timezonetracker.android.library)
     alias(libs.plugins.timezonetracker.hilt)
@@ -6,6 +9,35 @@ plugins {
 
 android {
     namespace = "uk.co.mhl.timezonetracker.core.datastore"
+}
+
+protobuf {
+    protoc {
+    }
+
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                register("java") {
+                    option("lite")
+                }
+                register("kotlin") {
+                    option("lite")
+                }
+            }
+        }
+    }
+}
+
+androidComponents {
+    onVariants(selector().all()) { variant ->
+        afterEvaluate {
+            val capName = variant.name.capitalized()
+            tasks.getByName<KotlinCompile>("ksp${capName}Kotlin") {
+                setSource(tasks.getByName("generate${capName}Proto").outputs)
+            }
+        }
+    }
 }
 
 dependencies {
--- core/datastore/src/main/java/uk/co/mhl/timezonetracker/core/datastore/di/DataStoreModule.kt
+++ core/datastore/src/main/java/uk/co/mhl/timezonetracker/core/datastore/di/DataStoreModule.kt
@@ -1,9 +1,18 @@
 package uk.co.mhl.timezonetracker.core.datastore.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.dataStoreFile
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import uk.co.mhl.timezonetracker.core.datastore.TimeZonePreferences
+import uk.co.mhl.timezonetracker.core.datastore.TimeZonePreferencesSerializer
 import javax.inject.Singleton
 
 @Module
@@ -12,7 +21,14 @@ object DataStoreModule {
 
     @Provides
     @Singleton
-    internal fun providesTimeZonePreferencesDataStore(): String {
-        return ""
-    }
+    internal fun providesTimeZonePreferencesDataStore(
+        @ApplicationContext context: Context,
+        timeZonePreferencesSerializer: TimeZonePreferencesSerializer,
+    ): DataStore<TimeZonePreferences> =
+        DataStoreFactory.create(
+            serializer = timeZonePreferencesSerializer,
+            scope = CoroutineScope(CoroutineScope(Dispatchers.Default).coroutineContext + Dispatchers.IO),
+        ) {
+            context.dataStoreFile("time_zone_preferences.pb")
+        }
 }

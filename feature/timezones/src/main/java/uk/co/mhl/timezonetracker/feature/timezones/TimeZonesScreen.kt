1ba829799a1924ce51ad2ed3d27e8b5573810c66
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-04-11 11:59:58 +1200

    Combined time and tracked city flows.

--- feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/TimeZonesScreen.kt
+++ feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/TimeZonesScreen.kt
@@ -2,17 +2,22 @@ package uk.co.mhl.timezonetracker.feature.timezones
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import uk.co.mhl.timezonetracker.core.designsystem.component.TimeZoneTrackerTopAppBar
 import uk.co.mhl.timezonetracker.core.designsystem.theme.TimeZoneTrackerTheme
+import uk.co.mhl.timezonetracker.core.model.City
 import uk.co.mhl.timezonetracker.feature.timezones.component.LocalTimeDisplay
 import uk.co.mhl.timezonetracker.feature.timezones.component.NewTimeZoneFloatingActionButton
 import uk.co.mhl.timezonetracker.feature.timezones.component.SavedTimeZoneItem
@@ -28,7 +33,7 @@ internal fun TimeZonesScreen(
 
     TimeZonesScreen(
         currentTime = uiState.currentTime,
-        savedTimeZones = emptyList(),
+        trackedCities = uiState.trackedCities,
         onNewTimeZoneClick = onNewTimeZoneClick,
         modifier = modifier,
     )
@@ -38,7 +43,7 @@ internal fun TimeZonesScreen(
 @Composable
 internal fun TimeZonesScreen(
     currentTime: Instant,
-    savedTimeZones: List<String>,
+    trackedCities: List<City>,
     onNewTimeZoneClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
@@ -53,12 +58,21 @@ internal fun TimeZonesScreen(
         Column(
             modifier = Modifier.padding(innerPadding),
         ) {
+            // TODO: Properly show SavedTimeZoneItems.
             LocalTimeDisplay(currentTime = currentTime)
-            SavedTimeZoneItem(
-                cityName = "Toronto",
-                offset = -5,
-                currentTime = currentTime,
-            )
+            LazyColumn {
+                items(trackedCities) { city ->
+                    Text(
+                        modifier = Modifier.padding(16.dp),
+                        text = city.name
+                    )
+                }
+            }
+//            SavedTimeZoneItem(
+//                cityName = "Toronto",
+//                offset = -5,
+//                currentTime = currentTime,
+//            )
         }
     }
 }
@@ -69,7 +83,14 @@ private fun TimeZonesScreenPreview() {
     TimeZoneTrackerTheme {
         TimeZonesScreen(
             currentTime = Instant.now(),
-            savedTimeZones = emptyList(),
+            trackedCities = listOf(
+                City(
+                    id = 0,
+                    name = "Toronto",
+                    country = "Canada",
+                    zoneId = "America/Toronto",
+                )
+            ),
             onNewTimeZoneClick = { },
         )
     }

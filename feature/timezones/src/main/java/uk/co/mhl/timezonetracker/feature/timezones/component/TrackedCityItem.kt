7a7c0af9c0e0d8b03f86011512a4fa1fcb4504f4
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-04-22 12:36:21 +1200

    Added content description string resource.

--- feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/component/TrackedCityItem.kt
+++ feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/component/TrackedCityItem.kt
@@ -18,10 +18,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import uk.co.mhl.timezonetracker.core.designsystem.theme.TimeZoneTrackerTheme
 import uk.co.mhl.timezonetracker.core.ui.zonedDateFormatted
+import uk.co.mhl.timezonetracker.feature.timezones.R
 import java.time.Instant
 import java.time.ZoneId
 
@@ -70,7 +72,7 @@ internal fun TrackedCityItem(
             ) {
                 Icon(
                     imageVector = Icons.Rounded.MoreVert,
-                    contentDescription = "TODO",
+                    contentDescription = stringResource(R.string.tracked_city_item_more),
                     tint = MaterialTheme.colorScheme.onSurfaceVariant,
                 )
             }

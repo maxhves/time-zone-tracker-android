177fc9c918e82b40c143be981c915558b34bc6aa
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-04-27 13:02:38 +1200

    Updated the design of TrackedCityItem.

--- feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/component/TrackedCityItem.kt
+++ feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/component/TrackedCityItem.kt
@@ -1,5 +1,6 @@
 package uk.co.mhl.timezonetracker.feature.timezones.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material3.Icon
@@ -18,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,27 +43,31 @@ internal fun TrackedCityItem(
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .background(
+                shape = MaterialTheme.shapes.medium,
+                color = MaterialTheme.colorScheme.surfaceContainerLow,
+            )
             .padding(
                 top = 16.dp,
                 bottom = 16.dp,
                 start = 16.dp,
-                end = 8.dp,
+                end = 4.dp,
             )
             .height(IntrinsicSize.Min),
     ) {
         Column(
             modifier = Modifier.weight(1f),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
         ) {
             Text(
                 text = city,
-                style = MaterialTheme.typography.titleMedium,
-                color = MaterialTheme.colorScheme.onBackground.copy(alpha = .75f)
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = .75f)
             )
             Text(
                 text = zonedDateFormatted,
-                style = MaterialTheme.typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onBackground,
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.onSurface,
             )
         }
         Box(

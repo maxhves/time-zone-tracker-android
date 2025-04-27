9e9ddbe2795759b0a5e66ec9b5e0d224ce7b9773
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-04-27 13:45:20 +1200

    Added ic_heart_minus icon.

--- feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/component/TrackedCityOptionsModalBottomSheet.kt
+++ feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/component/TrackedCityOptionsModalBottomSheet.kt
@@ -1,5 +1,6 @@
 package uk.co.mhl.timezonetracker.feature.timezones.component
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -8,8 +9,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -20,12 +19,13 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import uk.co.mhl.timezonetracker.core.designsystem.theme.TimeZoneTrackerTheme
 import uk.co.mhl.timezonetracker.feature.timezones.R
+import uk.co.mhl.timezonetracker.core.designsystem.R as RDesignSystem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -59,8 +59,8 @@ private fun TrackedCityOptionsModalBottomSheet(
     ) {
         TrackedCityOptionRow(
             onClick = onRemoveClick,
-            icon = Icons.Outlined.Delete,
-            label = R.string.remove_city_option_label,
+            iconRes = RDesignSystem.drawable.ic_heart_minus,
+            labelRes = R.string.remove_city_option_label,
         )
     }
 }
@@ -68,9 +68,10 @@ private fun TrackedCityOptionsModalBottomSheet(
 @Composable
 private fun TrackedCityOptionRow(
     modifier: Modifier = Modifier,
-    icon: ImageVector,
+    @DrawableRes
+    iconRes: Int,
     @StringRes
-    label: Int,
+    labelRes: Int,
     onClick: () -> Unit,
 ) {
     Row(
@@ -83,12 +84,12 @@ private fun TrackedCityOptionRow(
         horizontalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         Icon(
-            imageVector = icon,
+            painter = painterResource(iconRes),
             contentDescription = null,
             tint = MaterialTheme.colorScheme.onSurfaceVariant,
         )
         Text(
-            text = stringResource(label),
+            text = stringResource(labelRes),
             style = MaterialTheme.typography.bodyLarge,
             color = MaterialTheme.colorScheme.onSurface,
         )

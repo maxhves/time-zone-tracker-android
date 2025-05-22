e23a11d935d7c858df08f4eb9fba4eab42d63dea
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-05-22 07:48:57 +0100

    Setup test repository properties in test view model.

--- feature/timezones/src/test/java/uk/co/mhl/timezonetracker/feature/timezones/TimeZonesViewModelTest.kt
+++ feature/timezones/src/test/java/uk/co/mhl/timezonetracker/feature/timezones/TimeZonesViewModelTest.kt
@@ -1,10 +1,29 @@
 package uk.co.mhl.timezonetracker.feature.timezones
 
+import org.junit.Before
 import org.junit.Rule
+import uk.co.mhl.timezonetracker.core.testing.TestCityRepository
+import uk.co.mhl.timezonetracker.core.testing.TestTimeRepository
+import uk.co.mhl.timezonetracker.core.testing.TestUserDataRepository
 import uk.co.mhl.timezonetracker.core.testing.util.MainDispatcherRule
 
 class TimeZonesViewModelTest {
 
     @get:Rule
     val dispatcherRule = MainDispatcherRule()
+
+    private val currentTimeRepository = TestTimeRepository()
+    private val cityRepository = TestCityRepository()
+    private val userDataRepository = TestUserDataRepository()
+
+    private lateinit var viewModel: TimeZonesViewModel
+
+    @Before
+    fun setup() {
+        viewModel = TimeZonesViewModel(
+            currentTimeRepository = currentTimeRepository,
+            cityRepository = cityRepository,
+            userDataRepository = userDataRepository,
+        )
+    }
 }

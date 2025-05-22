e98f29a5fbd5192d224017ad7ae67b0d4b8687ce
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-05-22 08:08:30 +0100

    Added initial test functions.

--- feature/timezones/src/test/java/uk/co/mhl/timezonetracker/feature/timezones/TimeZonesViewModelTest.kt
+++ feature/timezones/src/test/java/uk/co/mhl/timezonetracker/feature/timezones/TimeZonesViewModelTest.kt
@@ -1,11 +1,18 @@
 package uk.co.mhl.timezonetracker.feature.timezones
 
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import uk.co.mhl.timezonetracker.core.testing.TestCityRepository
 import uk.co.mhl.timezonetracker.core.testing.TestTimeRepository
 import uk.co.mhl.timezonetracker.core.testing.TestUserDataRepository
 import uk.co.mhl.timezonetracker.core.testing.util.MainDispatcherRule
+import java.time.Instant
+import kotlin.test.assertEquals
 
 class TimeZonesViewModelTest {
 
@@ -25,5 +32,22 @@ class TimeZonesViewModelTest {
             cityRepository = cityRepository,
             userDataRepository = userDataRepository,
         )
+    }
+
+    @Test
+    fun whenInitialized_trackedCities_isEmpty() = runTest {
+        assertEquals(emptyList(), viewModel.trackedCitiesState.value)
+    }
+
+    @Test
+    fun currentTimeState_emitsCorrectTime() = runTest {
+        backgroundScope.launch(UnconfinedTestDispatcher()) { viewModel.currentTimeState.collect() }
+
+        val instantNow = Instant.now()
+        currentTimeRepository.sendTime(instantNow)
+
+        val currentTimeState = viewModel.currentTimeState.value
+
+        assertEquals(instantNow, currentTimeState)
     }
 }

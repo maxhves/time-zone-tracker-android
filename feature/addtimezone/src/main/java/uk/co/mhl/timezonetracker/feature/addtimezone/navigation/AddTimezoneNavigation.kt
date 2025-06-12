b73ebcc0c1ca80a72fd7e22386fcb55795863144
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-06-12 11:10:06 +0100

    Removed legacy navigation graph.

--- feature/addtimezone/src/main/java/uk/co/mhl/timezonetracker/feature/addtimezone/navigation/AddTimezoneNavigation.kt
+++ feature/addtimezone/src/main/java/uk/co/mhl/timezonetracker/feature/addtimezone/navigation/AddTimezoneNavigation.kt
@@ -1,54 +1,33 @@
 package uk.co.mhl.timezonetracker.feature.addtimezone.navigation
 
-import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptionsBuilder
-import androidx.navigation.compose.composable
 import androidx.navigation3.runtime.EntryProviderBuilder
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entry
 import kotlinx.serialization.Serializable
 import uk.co.mhl.timezonetracker.feature.addtimezone.AddTimeZoneScreen
 
 //region Route
 
-@Serializable data object AddTimeZoneRoute
+@Serializable data object AddTimeZone : NavKey
 
 //endregion
 
-//region Navigation
+//region Events
 
-fun NavController.navigateToAddTimeZone(navOptions: NavOptionsBuilder.() -> Unit = {}) {
-    navigate(route = AddTimeZoneRoute) {
-        navOptions()
-    }
-}
-
-fun SnapshotStateList<Any>.navigateToAddTimeZone() {
-    add(AddTimeZoneRoute)
+fun NavBackStack.navigateToAddTimeZone() {
+    add(AddTimeZone)
 }
 
 //endregion
 
 //region Graph
 
-fun NavGraphBuilder.addTimeZoneSection(
+fun EntryProviderBuilder<NavKey>.addTimeZonesNavigation(
     onBack: () -> Unit,
     onCityTracked: () -> Unit,
 ) {
-    composable<AddTimeZoneRoute> {
-        AddTimeZoneScreen(
-            onBack = onBack,
-            onCityTracked = onCityTracked,
-        )
-    }
-}
-
-fun EntryProviderBuilder<*>.addTimeZoneSection(
-    onBack: () -> Unit,
-    onCityTracked: () -> Unit,
-) {
-    entry<AddTimeZoneRoute> {
+    entry<AddTimeZone> {
         AddTimeZoneScreen(
             onBack = onBack,
             onCityTracked = onCityTracked,
--- feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/navigation/TimezonesNavigation.kt
+++ feature/timezones/src/main/java/uk/co/mhl/timezonetracker/feature/timezones/navigation/TimezonesNavigation.kt
@@ -1,47 +1,23 @@
 package uk.co.mhl.timezonetracker.feature.timezones.navigation
 
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.composable
-import androidx.navigation.navigation
 import androidx.navigation3.runtime.EntryProviderBuilder
+import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entry
 import kotlinx.serialization.Serializable
 import uk.co.mhl.timezonetracker.feature.timezones.TimeZonesScreen
 
 //region Route
 
-@Serializable data object TimeZonesRoute
-@Serializable data object TimeZonesBaseRoute
+@Serializable data object TimeZones : NavKey
 
 //endregion
 
 //region Graph
 
-fun NavGraphBuilder.timeZonesSection(
-    onNewTimeZoneClick: () -> Unit,
-    addTimeZoneDestination: NavGraphBuilder.() -> Unit,
-) {
-    navigation<TimeZonesBaseRoute>(startDestination = TimeZonesRoute) {
-        composable<TimeZonesRoute> {
-            TimeZonesScreen(
-                onNewTimeZoneClick = onNewTimeZoneClick,
-            )
-        }
-        addTimeZoneDestination()
+fun EntryProviderBuilder<NavKey>.timeZonesNavigation(onNewTimeZoneClick: () -> Unit) {
+    entry<TimeZones> {
+        TimeZonesScreen(onNewTimeZoneClick = onNewTimeZoneClick)
     }
-}
-
-
-fun EntryProviderBuilder<Any>.timeZonesSection(
-    onNewTimeZoneClick: () -> Unit,
-    addTimeZoneDestination: EntryProviderBuilder<Any>.() -> Unit,
-) {
-    entry<TimeZonesBaseRoute> {
-        TimeZonesScreen(
-            onNewTimeZoneClick = onNewTimeZoneClick,
-        )
-    }
-    addTimeZoneDestination()
 }
 
 //endregion
